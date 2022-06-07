6740bd9f2d1876909118fd43ef5f9ebc223a73d6
UT0P1C <ljonham@gmail.com>
2022-06-07 19:26:52 -0300

    Crud do carrinho de compras (irei me matar em breve)

--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -1,7 +1,87 @@
+import Cart from "../models/Cart";
+
 class CartsController {
 	async index (req, res) {
-		return res.status(200).json({foo: "bar"})
+
+		try {
+			const carts = await Cart.find();
+			return res.status(200).json(carts);
+		} 
+		catch (err) {
+			console.log(err);
+			return res.status(418).json({message: "i'm a teapot"});
+		}
+	}
+
+	//create card
+
+	async create(req, res) {
+		try {
+			const {code, price} = req.body;
+
+			const cart = await Cart.create({code, price});
+
+			return res.status(201).json(cart);
+		} 
+		catch (err) {
+			console.log(err);
+			return res.status(418).json({message: "I'm a teapot"});
+		}
+	}
+
+
+	//update card
+
+	async update(req, res) {
+		try {
+			
+			const { id } = req.params;
+
+			const {code, price} = req.body;
+
+			const cart = await Cart.findById(id);
+
+			if(!cart){
+				return res.status(404).json({message: "Error: Product not found"});
+			}
+
+			await Cart.updateOne({code, price});
+
+			return res.status(200).json({message: "Product updated successfully"});
+
+
+		} 
+		catch (err) {
+			console.log(err);
+			return res.status(418).json({message: "I'm a teapot"});
+		}
+	}
+
+	//Delete cart
+
+	async delete(req, res) {
+		try {
+			const {id} = req.params;
+
+			const cart = await Cart.findById(id);
+
+			if(!cart){
+				return res.status(404).json({message: "Error: Product not found"});
+			}
+
+			await cart.deleteOne();
+
+			return res.status(200).json({message: "Product updated successfully"});
+			
+		}
+		catch (err) {
+			console.log(err);
+			return res.status(418).json({message: "I'm a teapot"});
+		}
 	}
 }
+
+
+
 
 export default new CartsController();
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,8 @@ import CartsController from "./controllers/CartsController";
 const routes = new Router();
 
 routes.get('/cart', CartsController.index);
+routes.post('/cart', CartsController.create);
+routes.put('/cart/:id', CartsController.update);
+routes.delete('/cart/:id', CartsController.delete);
 
 export default routes;
