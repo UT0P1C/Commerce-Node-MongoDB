03d4037b372c26d4426c82b4914695d841f9959a
UT0P1C <ljonham@gmail.com>
2022-06-08 20:09:45 -0300

    validação de celular e cpf/cnpj

--- src/controllers/TransactionsController.js
+++ src/controllers/TransactionsController.js
@@ -1,7 +1,78 @@
-import Transaction from "../models/Transaction";
+
+import * as Yup from "yup";
+
+import parsePhoneNumber from "libphonenumber-js";
+
+import {cpf, cnpj} from "cpf-cnpj-validator";
 
 class TransactionsController {
+	async create(req, res) {
+		try {
 
+			//transformando o body do post em um schema para pós validação
+
+			const {
+				cartCode,
+				paymentType,
+				installments,
+				customerName,
+				customerEmail,
+				customerMobile,
+				customerDocument,
+				billingAddress,
+				billingNumber,
+				billingNeighborhood,
+				billingCity,
+				billingState,
+				billingZipCode,
+				creditCardNumber,
+				creditCardExpiration,
+				creditCardHolderName,
+				creditCardCvv
+			} = req.body;
+
+			//utilizando o yup para validar o schema
+
+			const schema = Yup.object({
+				cartCode: Yup.string().required(),
+				paymentType: Yup.mixed().oneOf(["credit_card", "billet"]).required(),
+				installments: Yup.number().min(1)
+				.when("paymentType", (paymentType, schema) => paymentType === "credit_card" ? schema.max(12) : schema.max(1)),
+				customerName: Yup.string().required().min(3),
+				customerEmail: Yup.string().required().email(),
+
+				//validando o numero de celular com a biblioteca libphonenumber-js
+				customerMobile: Yup.string()
+				.required()
+				.test("is-valid-mobile", "${path} is not a valid phone number!", 
+				(value) => parsePhoneNumber(value, "BR").isValid()
+				),
+
+				//validando o cpf/cnpj com a biblioteca cpf-cnpj-validator
+				customerDocument: Yup.string()
+				.required()
+				.test("is-valid-document", 
+				"${path} is not a valid Document!", 
+				(value) => cpf.isValid(value) || cnpj.isValid(value)
+				),
+
+
+				
+
+			});
+
+			if(!(await schema.isValid(req.body))){
+
+				return res.status(400).json({error: "error on validate schema"});
+
+			}
+
+			return res.status(200).json({message: "ok!"});
+		} catch (err) {
+			console.log(err);
+			return res.status(418).json({error: "I'm a teapot"});
+		}
+	}
 }
 
 export default new TransactionsController();
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@ import { Router } from "express";
 
 import CartsController from "./controllers/CartsController";
 
+import TransactionsController from "./controllers/TransactionsController";
+
 const routes = new Router();
 
 routes.get('/cart', CartsController.index);
 routes.post('/cart', CartsController.create);
 routes.put('/cart/:id', CartsController.update);
 routes.delete('/cart/:id', CartsController.delete);
+
+
+routes.post('/transactions', TransactionsController.create);
 
 export default routes;
